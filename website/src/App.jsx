6654012316d7babd2035a6c917d0ed4c112ab4eb
Sam <samglev@gmail.com>
2025-04-23 13:21:36 -0400

    Got some more stuff set up hopefully

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
